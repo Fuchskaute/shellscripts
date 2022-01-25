8aa0894d6c8560046157da721d218a61e5053c1a
snaiwaz <87344188+snaiwaz@users.noreply.github.com>
2022-01-25 23:40:57 +0100

    Update screenshot.sh

--- screenshot.sh
+++ screenshot.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
+#This script requires dmenu to run 
+
 choices="current_window\nboth_screens\nselection"
 
 chosen=$(echo -e "$choices" | dmenu -i)
 
 filename=$(echo " " | dmenu -i -p "Filename: ")
-
-#dmenu -i read -p "filename: " filename
 
 case "$chosen" in
     both_screens) maim -x :0 -B -u ~/Screenshots/"$filename";;
